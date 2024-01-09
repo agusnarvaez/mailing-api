27da33e7950593642299ba0c4c1d1e70e491feec
agusnarvaez <agus.narvaez@outlook.com>
2024-01-08 23:17:48 -0300

    Agrego método setear cc en mail

--- src/models/mail.js
+++ src/models/mail.js
@@ -1,10 +1,14 @@
 
 export class Mail{
+    cc = ''
     constructor(from, to, subject,message, html){
         this.from = from
         this.to = to
         this.subject = subject
         this.body = message
         this.html = html
+    }
+    setCC(cc){
+        this.cc = cc
     }
 }
